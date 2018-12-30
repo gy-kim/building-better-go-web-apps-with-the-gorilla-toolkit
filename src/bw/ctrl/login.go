20e7e34b3507e8d3c7ac4a67b91c2e1da95a307e
Geunyoung Kim <kgysmart@gmail.com>
2018-12-29 16:57:29 -0800

    Add sample codes

---
+++ src/bw/ctrl/login.go
@@ -0,0 +1,16 @@
+package ctrl
+
+import (
+	"bw/vm"
+	"html/template"
+	"net/http"
+)
+
+type loginController struct {
+	loginTemplate *template.Template
+}
+
+func (lc *loginController) GetLogin(w http.ResponseWriter, r *http.Request) {
+	vmodel := vm.Base{}
+	lc.loginTemplate.Execute(w, vmodel)
+}
